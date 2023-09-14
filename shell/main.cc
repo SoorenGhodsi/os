80eaf11b8863cd4b065c4b66b3120f1dc617a425
farukyaylagul <urj5rb@virginia.edu>
2023-09-14 00:56:46 -0400

    shell part  2

--- shell/main.cc
+++ shell/main.cc
@@ -8,8 +8,76 @@
 
 using namespace std;
 
+void execute_pipeline(const vector<vector<const char*>> &commands, const string &input_file, const string &output_file) {
+    int fds[2];
+    pid_t pid;
+    int in_fd = 0; 
+    bool input_redirection = !input_file.empty();
+    bool output_redirection = !output_file.empty();
+
+    for(size_t i = 0; i < commands.size(); i++) {
+        
+        // Check if we can create a new pipe
+        if (i != commands.size() - 1) {
+            if (pipe(fds) == -1) {
+                cerr << "Error: Failed to create a pipe. Maybe too many open files?" << endl;
+                exit(EXIT_FAILURE);
+            }
+        }
+
+        if ((pid = fork()) == 0) {
+            if (input_redirection && i == 0) {  
+                int in_fd = open(input_file.c_str(), O_RDONLY);
+                if(in_fd == -1) {
+                    cerr << "Error: Unable to open input file." << endl;
+                    exit(EXIT_FAILURE);
+                }
+                dup2(in_fd, STDIN_FILENO);
+                close(in_fd); 
+            } else if (i != 0) {
+                dup2(in_fd, STDIN_FILENO); 
+                close(in_fd); 
+            }
+
+            if(i != commands.size() - 1) {
+                dup2(fds[1], STDOUT_FILENO);
+                close(fds[1]);
+                close(fds[0]);
+            } else if (output_redirection) { 
+                int out_fd = open(output_file.c_str(), O_WRONLY | O_CREAT | O_TRUNC, 0666);
+                if(out_fd == -1) {
+                    cerr << "Error: Unable to open output file." << endl;
+                    exit(EXIT_FAILURE);
+                }
+                dup2(out_fd, STDOUT_FILENO);
+                close(out_fd);
+            }
+            
+            execvp(commands[i][0], (char* const*)commands[i].data());
+            cerr << "Error: Command not found." << endl;
+            exit(EXIT_FAILURE);
+        } else {
+            if (i != commands.size() - 1) {
+                close(fds[1]); 
+                in_fd = fds[0];
+            }
+            
+            int status;
+            waitpid(pid, &status, 0); 
+
+            if (WIFEXITED(status))
+                cout << commands[i][0] << " exit status: " << WEXITSTATUS(status) << endl; 
+            else
+                cerr << "Error: Child process did not exit correctly." << endl;
+
+            if (i == commands.size() - 1 && in_fd != 0) {
+                close(in_fd);
+            }
+        }
+    }
+}
+
 void parse_and_run_command(const string &command) {
-    // Declare variables and initialize redirection booleans
     vector<string> tokens;
     string input_file, output_file;
     bool input_redirection = false, output_redirection = false;
@@ -24,28 +92,33 @@ void parse_and_run_command(const string &command) {
     if(pos < command.length())
         tokens.push_back(command.substr(pos));
 
-    // Check if no command was entered
     if (tokens.empty()) {
         cerr << "Error: No command entered. " << endl;
         return;
     }
 
-    // Handling 'exit' command
     if (tokens[0] == "exit")
         exit(0);
 
-    // Special invalid exec command
     if (tokens[0] == "test/invalid-exec")
         cerr << "Error: Invalid exec." << endl;
 
-    // Parse tokens for redirections
-    // Store other tokens in c_tokens
-    vector<const char*> c_tokens;
+    vector<vector<const char*>> pipeline_commands;
+    vector<const char*> current_command;
+
     for(auto it = tokens.begin(); it != tokens.end(); ++it) {
-        if(*it == "<") {
+        if(*it == "|") {
+            if(current_command.empty()) {
+                cerr << "Error: Invalid command around |" << endl;
+                return;
+            }
+            current_command.push_back(nullptr);
+            pipeline_commands.push_back(current_command);
+            current_command.clear();
+        } else if(*it == "<") {
             input_redirection = true;
             ++it;
-            if(it != tokens.end() && *it != ">" && *it != "<")
+            if(it != tokens.end() && *it != ">" && *it != "<" && *it != "|")
                 input_file = *it;
             else {
                 cerr << "Error: Invalid command, no input file specified." << endl;
@@ -54,26 +127,41 @@ void parse_and_run_command(const string &command) {
         } else if(*it == ">") {
             output_redirection = true;
             ++it;
-            if(it != tokens.end() && *it != ">" && *it != "<")
+            if(it != tokens.end() && *it != ">" && *it != "<" && *it != "|")
                 output_file = *it;
             else {
                 cerr << "Error: Invalid command, no output file specified." << endl;
                 return;
             }
-        } else // Non-redirection token
-            c_tokens.push_back(it->c_str()); 
+        } else {
+            current_command.push_back(it->c_str());
+        }
     }
 
-    // Case where no valid tokens are found
-    if(c_tokens.empty()) {
+    if(!current_command.empty()) {
+        current_command.push_back(nullptr);
+        pipeline_commands.push_back(current_command);
+    }   
+    else if (!pipeline_commands.empty()) {
+    cerr << "Error: Invalid command after |" << endl;
+    return;
+    }
+
+    if (pipeline_commands.empty()) {
         cerr << "Error: Invalid command." << endl;
+    return;
+    }
+
+    if(pipeline_commands.size() > 1) {
+        execute_pipeline(pipeline_commands, input_file, output_file);
+
         return;
     }
-    c_tokens.push_back(nullptr);
+
+    auto& c_tokens = pipeline_commands[0];
 
     pid_t pid;
     if ((pid = fork()) == 0) { // Child process
-        // Redirect stdin --> input file
         if(input_redirection) {
             int in_fd = open(input_file.c_str(), O_RDONLY);
             if(in_fd == -1) {
@@ -84,7 +172,6 @@ void parse_and_run_command(const string &command) {
             close(in_fd);
         }
 
-        // Redirect stdout --> output file
         if(output_redirection) {
             int out_fd = open(output_file.c_str(), O_WRONLY | O_CREAT | O_TRUNC, 0666);
             if(out_fd == -1) {
@@ -95,27 +182,19 @@ void parse_and_run_command(const string &command) {
             close(out_fd);
         }
 
-        // Execute the command 
-        int retval = execv(c_tokens[0], const_cast<char* const*>(c_tokens.data()));
-        if (retval < 0) {
-            cerr << "Error: Command not found." << endl; // This line is executed only if execvp fails
-            exit(EXIT_FAILURE);
-        }
+        execvp(c_tokens[0], (char* const*)c_tokens.data());
+        cerr << "Error: Command not found." << endl;
+        exit(EXIT_FAILURE);
 
     } else if (pid > 0) { // Parent process
-        // Wait for the child process to finish and get its status
         int status;
         waitpid(pid, &status, 0);
-
-        // Print child exit status
         if (WIFEXITED(status))
             cout << c_tokens[0] << " exit status: " << WEXITSTATUS(status) << endl; 
         else
             cerr << "Error: Child process did not exit correctly." << endl;
-            
     } else { // Fork fails if pid is negative
         cerr << "Error: Fork failed." << endl;
-        return;
     }
 }
 
