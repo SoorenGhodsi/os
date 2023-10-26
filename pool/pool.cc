928da34793a701f877606a4a3f833f566bf21397
SoorenGhodsi <sooren8@email.com>
2023-10-25 22:53:18 -0400

    pool complete

--- pool/pool.cc
+++ pool/pool.cc
@@ -1,19 +1,77 @@
 #include "pool.h"
 
 Task::Task() {
+
 }
 
 Task::~Task() {
+
 }
 
-ThreadPool::ThreadPool(int num_threads) {
+void* ThreadPool::Worker(void* arg) {
+    ThreadPool* pool = (ThreadPool*) arg;
+
+    while (true) {
+        pthread_mutex_lock(&pool->queueMutex);
+        while (pool->taskQueue.empty() && !pool->stopFlag.load())
+            pthread_cond_wait(&pool->condVar, &pool->queueMutex);
+
+        if (pool->stopFlag.load() && pool->taskQueue.empty()) {
+            pthread_mutex_unlock(&pool->queueMutex);
+            break;
+        }
+
+        TaskWrapper taskWrapper = pool->taskQueue.front();
+        pool->taskQueue.pop_front();
+        pthread_mutex_unlock(&pool->queueMutex);
+
+        taskWrapper.task->Run();
+
+        pthread_mutex_lock(&pool->queueMutex);
+        pool->taskFinished[taskWrapper.name] = true;
+        pthread_cond_broadcast(&pool->condVar);
+        pthread_mutex_unlock(&pool->queueMutex);
+
+        delete taskWrapper.task;
+    }
+    return nullptr;
 }
 
-void ThreadPool::SubmitTask(const std::string &name, Task* task) {
+ThreadPool::ThreadPool(int num_threads) : num_threads(num_threads), stopFlag(false) {
+    pthread_mutex_init(&queueMutex, nullptr);
+    pthread_cond_init(&condVar, nullptr);
+
+    threads = new pthread_t[num_threads];
+    for (int i = 0; i < num_threads; ++i)
+        pthread_create(&threads[i], nullptr, Worker, this);
 }
 
-void ThreadPool::WaitForTask(const std::string &name) {
+ThreadPool::~ThreadPool() {
+    delete[] threads;
+    pthread_mutex_destroy(&queueMutex);
+    pthread_cond_destroy(&condVar);
+}
+
+void ThreadPool::SubmitTask(const string &name, Task *task) {
+    pthread_mutex_lock(&queueMutex);
+    taskQueue.push_back(TaskWrapper(name, task));
+    pthread_cond_signal(&condVar);
+    pthread_mutex_unlock(&queueMutex);
+}
+
+void ThreadPool::WaitForTask(const string &name) {
+    pthread_mutex_lock(&queueMutex);
+    while (!taskFinished[name])
+        pthread_cond_wait(&condVar, &queueMutex);
+
+    taskFinished.erase(name);
+    pthread_mutex_unlock(&queueMutex);
 }
 
 void ThreadPool::Stop() {
+    stopFlag.store(true);
+    pthread_cond_broadcast(&condVar);
+
+    for (int i = 0; i < num_threads; ++i)
+        pthread_join(threads[i], nullptr);
 }
--- pool/pool.h
+++ pool/pool.h
@@ -1,27 +1,47 @@
-#ifndef POOL_H_
+#ifndef POOL_H
+#define POOL_H
+
 #include <string>
 #include <pthread.h>
+#include <deque>
+#include <map>
+#include <atomic>
+
+using namespace std;
 
 class Task {
 public:
     Task();
     virtual ~Task();
-
     virtual void Run() = 0;  // implemented by subclass
 };
 
+struct TaskWrapper {
+    string name;
+    Task* task;
+    TaskWrapper(const string &name, Task *task) : name(name), task(task) {}
+};
+
 class ThreadPool {
+private:
+    int num_threads;
+    deque<TaskWrapper> taskQueue;
+    map<string, bool> taskFinished;
+
+    pthread_t* threads;
+    pthread_mutex_t queueMutex;
+    pthread_cond_t condVar;
+    atomic<bool> stopFlag;
+
+    static void* Worker(void* arg);
+
 public:
     ThreadPool(int num_threads);
+    ~ThreadPool();
 
-    // Submit a task with a particular name.
-    void SubmitTask(const std::string &name, Task *task);
- 
-    // Wait for a task by name, if it hasn't been waited for yet. Only returns after the task is completed.
-    void WaitForTask(const std::string &name);
-
-    // Stop all threads. All tasks must have been waited for before calling this.
-    // You may assume that SubmitTask() is not caled after this is called.
+    void SubmitTask(const string &name, Task *task);
+    void WaitForTask(const string &name);
     void Stop();
 };
+
 #endif
